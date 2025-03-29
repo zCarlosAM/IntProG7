fca1e8cf5e4c8338784e3e0102ae97003d4c6d31
zCarlosAM <caaguirre@uamv.edu.ni>
2025-03-28 18:51:16 -0600

    Añadí los archivos ejemplo1.py y ejemplo3.py

---
+++ ejemplo1.py
@@ -0,0 +1,3 @@
+number = 7
+print("El número que posee la variable es", number)
+print("El tipo de dato de la variable es", type(number))
---
+++ ejemplo3.py
@@ -0,0 +1,3 @@
+# Leer el nombre de una persona y saludarla
+name = input("¿Cuál es tu nombre? ")
+print("Hola,", name, ", mucho gusto.")
